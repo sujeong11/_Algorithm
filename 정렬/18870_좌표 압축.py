b49911b659f303c74d9c45a726335579406ae21b
sujeong11 <qalzm7351@gmail.com>
2022-07-05 01:29:44 +0900

    정렬

--- 정렬/18870_좌표 압축.py
+++ 정렬/18870_좌표 압축.py
@@ -1,8 +1,14 @@
-# 언어 : Python, (성공/실패) : 1/1, 메모리 : 51616KB, 시간 : 4396ms
+# 언어 : Python, (성공/실패) : 1/1, 메모리 : 156812KB, 시간 : 2208ms
 
 import sys
 
 N = int(sys.stdin.readline())
+x = list(map(int, sys.stdin.readline().split()))
 
-for i in range(N):
-    x = list(map(int, sys.stdin.readline().split()))
+x_sort = list(sorted(set(x)))
+
+# 정렬 후 작은 값부터 인덱스를 부여
+dic = {x_sort[i]: i for i in range(len(x_sort))}
+
+for i in x:
+    print(dic[i], end=' ')

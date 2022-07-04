c2ce8314027e4ed764c52dcb8786aae09e1974bc
sujeong11 <qalzm7351@gmail.com>
2022-07-05 01:11:38 +0900

    조건문

--- 조건문/2480_주사위 세개.py
+++ 조건문/2480_주사위 세개.py
@@ -1,4 +1,4 @@
-# 언어: Python, (성공/실패) : 1/1, 메모리 : 30840KB, 시간 : 68ms
+# 언어 : Python, (성공/실패) : 1/1, 메모리 : 30840KB, 시간 : 68ms
 
 A, B, C = map(int, input().split())
 
--- 조건문/2525_오븐 시계.py
+++ 조건문/2525_오븐 시계.py
@@ -1,4 +1,4 @@
-# 언어: Python, (성공/실패) : 1/1, 메모리 : 30840KB, 시간 : 68ms
+# 언어 : Python, (성공/실패) : 1/1, 메모리 : 30840KB, 시간 : 68ms
 
 H, M = map(int, input().split())
 timer = int(input())

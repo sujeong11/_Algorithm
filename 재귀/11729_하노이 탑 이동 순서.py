7eab30021da5f48ee9573dc49272387cd56f1b97
sujeong11 <qalzm7351@gmail.com>
2022-07-05 01:15:27 +0900

    재귀

--- 재귀/11729_하노이 탑 이동 순서.py
+++ 재귀/11729_하노이 탑 이동 순서.py
@@ -4,9 +4,9 @@ def hanoi(n, a, b, c): # 원반 개수, 시작, 보조, 목표
     if n == 1:
         print(a, c) # 바로 목표로
     else:
-        hanoi(n - 1, a, c, b) # 더 작은 것을 보조 위치로
-        print(a, c) # 다음으로 작은 것은 바로 목표 위치로
-        hanoi(n - 1, b, a, c) # 더 작은 것을 목표 위치로
+        hanoi(n - 1, a, c, b) # 더 작은 것을 보조 위치로 이동
+        print(a, c) # 다음으로 작은 것은 바로 목표 위치로 이동
+        hanoi(n - 1, b, a, c) # 더 작은 것을 목표 위치로 이동
 
 sum = 1
 

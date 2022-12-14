332a500adc0dcc8832384197ad930b5f7d846c88
sujeong11 <qalzm7351@gmail.com>
2022-12-14 11:10:14 +0900

    2407(조합)

--- 동적계획법/2407.py
+++ 동적계획법/2407.py
@@ -1,5 +1,11 @@
-import math
+import sys, math
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
+print(math.factorial(n) // (math.factorial(m) * math.factorial(n-m)))
 
-print(math.comb(n, m))
+# import math
+#
+# n, m = map(int, input().split())
+#
+# print(math.comb(n, m))

d4dfecdcfaafbe3662b8cc7d3d730ea04b4fa453
sujeong11 <qalzm7351@gmail.com>
2023-01-16 11:04:49 +0900

    15649(N과 M(1))

--- 백트래킹/15649.py
+++ 백트래킹/15649.py
@@ -10,9 +10,9 @@ def back_tracking():
         print(" ".join(map(str, result)))
         return
     for i in range(1, N + 1):
-        if i not in result:
+        if i not in result: # 중복 확인
             result.append(i)
             back_tracking() # 재귀
-            result.pop()
+            result.pop() # 다음 수로 넘어가도록
 
 back_tracking()

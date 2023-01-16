e7673e6f07e1413a4f6b16c6f5a5343f82f777ed
sujeong11 <qalzm7351@gmail.com>
2023-01-16 11:12:16 +0900

    15650(N과 M(2))

---
+++ 백트래킹/15650.py
@@ -0,0 +1,18 @@
+# 내장함수 permutations으로 풀이 가능
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+result = []
+
+def back_tracking(start):
+    if len(result) == M:
+        print(" ".join(map(str, result)))
+        return
+    for i in range(start, N + 1):
+        if i not in result:
+            result.append(i)
+            back_tracking(i + 1) # 앞의 숫자가 뒤의 숫자보다 큰 경우 제외하도록
+            result.pop()
+
+back_tracking(1)

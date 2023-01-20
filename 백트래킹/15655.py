410b6539340d7cb09898ae9193768124698bfe46
sujeong11 <qalzm7351@gmail.com>
2023-01-20 22:00:15 +0900

    15655(N과 M(6))

---
+++ 백트래킹/15655.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+L = sorted(list(map(int, input().split())))
+result = []
+
+def back_tracking(start):
+    if len(result) == M:
+        print(" ".join(map(str, result)))
+        return
+    for i in range(start, N):
+        if L[i] not in result:
+            result.append(L[i])
+            back_tracking(i + 1)
+            result.pop()
+
+back_tracking(0)
